861cfa706f472981ca0cc18b4ba5c63cc13ad792
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-12-16 17:39:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 if [-d "/opt/homebrew"]; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "Please restart your terminal and restart the command"
+  exit 0
 fi
 
 brew install python
