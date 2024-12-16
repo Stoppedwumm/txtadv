0abf4294d4798f599586db4e4d49b2a8c028fa74
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-12-16 18:23:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-if [ -d "/opt/homebrew" ]; then
+HOMEBREW_EXISTS=$(which brew)
+
+if [ ! -z "$HOMEBREW_EXISTS" ]; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "Please restart your terminal and restart the command"
   exit 0
